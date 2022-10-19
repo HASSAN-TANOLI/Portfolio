581f65b9e24a3105bd9362fb6ef66f6128774cad
HASSAN-TANOLI <hassantanoli309@gmail.com>
2022-10-20 02:06:07 +0500

    working in About section

---
+++ frontend_react/src/client.js
@@ -0,0 +1,14 @@
+import sanityClient from "@sanity/client";
+import imgUrlBuilder from "@sanity/image-url";
+import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
+
+export const client = sanityClient({
+  projectId: process.env.REACT_APP_SANITY_ID,
+  dataset: "production",
+  apiVersion: "2022-10-20",
+  useCdn: "true",
+  token: process.env.REACT_APP_SANITY_TOKEN,
+});
+
+const builder = ImageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);

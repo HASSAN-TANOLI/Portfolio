95495861b2f4ab45a328288997ed8b78b76f7da0
HASSAN-TANOLI <hassantanoli309@gmail.com>
2022-10-31 14:06:48 +0500

    Second last commit

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
-import "./Navbar.scss";
+
 import { images } from "../../constants";
-import { useState } from "react";
-export const Navbar = () => {
+import "./Navbar.scss";
+
+const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="app__navbar">
-      <div className="app__navbar__logo">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
-      <ul className="app__navbar__links">
-        {["Home", "About", "Contact", "Work", "Skills"].map((item) => (
+      <ul className="app__navbar-links">
+        {["home", "about", "work", "skills", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
-            <a href={`#${item}`}> {item} </a>
+            <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
@@ -31,11 +32,10 @@ export const Navbar = () => {
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {["Home", "About", "Contact", "Work", "Skills"].map((item) => (
+              {["home", "about", "work", "skills", "contact"].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {" "}
-                    {item}{" "}
+                    {item}
                   </a>
                 </li>
               ))}

e8d77cbe6b80e098c2094de0f8c9d1d1899fa6b6
HASSAN-TANOLI <hassantanoli309@gmail.com>
2022-11-03 15:11:05 +0500

    finall commit

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { BsGithub, BsTwitter } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a
+        href="https://www.linkedin.com/in/muhammad-hassan-b008ba23a/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedinIn />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a
+        href="https://github.com/HASSAN-TANOLI"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsGithub />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a
+        href="https://twitter.com/hassant59823121"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsTwitter />
+      </a>
     </div>
   </div>
 );
